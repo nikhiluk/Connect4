37937a4b730abc7c77a093015a13cfc45098b962
Nikhil Sharma <nikhil.sharma@sky.uk>
2021-03-23 09:38:44 +0000

    add repl

--- src/main/java/uk/co/interviewprep3/Connect4Game.java
+++ src/main/java/uk/co/interviewprep3/Connect4Game.java
@@ -1,5 +1,9 @@
 package uk.co.interviewprep3;
 
+import java.util.Scanner;
+
+import static java.lang.String.format;
+
 public class Connect4Game {
 
 
@@ -19,5 +23,23 @@ public class Connect4Game {
         gameBoard.addCounter(player, columnToEnter);
         gameBoard.printState();
         return gameBoard.checkConsecutiveVertical(player) || gameBoard.checkConsecutiveHorizontal(player);
+    }
+
+    public static void main(String[] args) {
+        GameBoard gameBoard = new GameBoard();
+        Connect4Game connect4Game = new Connect4Game(gameBoard);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            final String[] input = scanner.next().split(",");
+            final String playerToken = input[0];
+            final int column = Integer.parseInt(input[1]);
+            final boolean winner = connect4Game.move(playerToken, column);
+            if (winner) {
+                System.out.println("Player " + playerToken + " has won");
+                break;
+            }
+        }
+
     }
 }
